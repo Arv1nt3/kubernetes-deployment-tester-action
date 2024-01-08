a130b2e7e1cfc9188769624897c53f71d3b9eb2f
Alexandru Arvinte <arvintealexandru1@gmail.com>
2024-01-09 00:51:11 +0200

    Trying to fix file

--- create_deployment.py
+++ create_deployment.py
@@ -7,6 +7,10 @@ def get_env_var(env_var, required, default=None):
         raise ValueError(f"Required environment variable '{env_var}' is missing.")
     return value if value else default
 
+def create_file_from_string(file_path, file_content):
+    with open(file_path, 'w') as file:
+        file.write(file_content)
+
 def create_deployment_yaml(image_name, command, args, env_vars, port, path, initial_delay_seconds, period_seconds):
     deployment = {
         "apiVersion": "apps/v1",
@@ -64,6 +68,48 @@ def create_deployment_yaml(image_name, command, args, env_vars, port, path, init
     with open('deployment.yaml', 'w') as file:
         yaml.dump(deployment, file)
 
+sa_yaml_content = """
+
+apiVersion: v1
+
+kind: ServiceAccount
+
+metadata:
+
+  name: test
+
+"""
+
+
+
+crb_yaml_content = """
+
+apiVersion: rbac.authorization.k8s.io/v1
+
+kind: ClusterRoleBinding
+
+metadata:
+
+  name: test
+
+subjects:
+
+- kind: ServiceAccount
+
+  name: test
+
+  namespace: default
+
+roleRef:
+
+  kind: ClusterRole
+
+  name: cluster-admin
+
+  apiGroup: rbac.authorization.k8s.io
+
+"""
+
 # Read inputs from environment variables
 image_name = get_env_var('INPUT_IMAGE_NAME', True)
 command = get_env_var('INPUT_COMMAND', False)
@@ -75,3 +121,5 @@ initial_delay_seconds = int(get_env_var('INPUT_INITIALDELAYSECONDS', False, 5))
 period_seconds = int(get_env_var('INPUT_PERIODSECONDS', False, 10))
 
 create_deployment_yaml(image_name, command, args, env_vars, port, path, initial_delay_seconds, period_seconds)
+create_file_from_string('sa.yaml', sa_yaml_content.strip())
+create_file_from_string('crb.yaml', crb_yaml_content.strip())
